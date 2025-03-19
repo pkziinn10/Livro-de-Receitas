987a9609b54a31bdb4f29a33eaca79a52616487c
pkziinn10 <pedrinhokauan824@gmail.com>
2025-03-18 22:24:51 -0300

    Criando a entidade usuario no dominio

---
+++ src/backend/MyRecipeBook.Domain/Entities/EntitiyBase.cs
@@ -0,0 +1,8 @@
+namespace MyRecipeBook.Domain.Entities;
+
+public class EntityBase 
+{
+    public long Id { get; set; }
+    public bool Active { get; set; } = true;
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+}
